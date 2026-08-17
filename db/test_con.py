16be1e97ad171b2fef5b6af1c077dbe4e6124d0e
Liam <liamboyle637@outlook.com>
2026-08-17 14:51:35 -0400

    dumb fix 01

--- db/test_con.py
+++ db/test_con.py
@@ -1,5 +1,16 @@
 #Database connection testing
 from db.session import SessionLocal
-from models import SwingMetrics
-from models import clip
+from db.models import Swing
+import datetime
 
+session = SessionLocal()
+
+new_swing = Swing(date = datetime.date.today(), notes = "test swing from offline work")
+session.add(new_swing)
+session.commit()
+
+result = session.query(Swing).all()
+for swing in result:
+    print(swing.id, swing.date, swing.notes)
+
+session.close()
