e8c08118e68d0577955f59fb39e345908faaec43
Liam <liamboyle637@outlook.com>
2026-08-17 22:12:05 -0400

    p6c1 early debug on db (models,session,test,pg)

--- db/test_con.py
+++ db/test_con.py
@@ -1,15 +1,15 @@
 #Database connection testing
 from db.session import SessionLocal
-from db.models import Swing
+from db.models import Swings
 import datetime
 
 session = SessionLocal()
 
-new_swing = Swing(date = datetime.date.today(), notes = "test swing from offline work")
+new_swing = Swings(date = datetime.date.today(), notes = "test swing from offline work")
 session.add(new_swing)
 session.commit()
 
-result = session.query(Swing).all()
+result = session.query(Swings).all()
 for swing in result:
     print(swing.id, swing.date, swing.notes)
 
