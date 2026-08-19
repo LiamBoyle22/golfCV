7c685d0d757dcdf23d71e74db927d0a11d3e2dc8
Liam <liamboyle637@outlook.com>
2026-08-19 15:12:10 -0400

    p6c4 adding testing for only one human model over many

--- scratch/test_pose.py
+++ scratch/test_pose.py
@@ -3,14 +3,27 @@ from ultralytics import YOLO
 model = YOLO("yolov8n-pose.pt")  # load a pretrained model (recommended for training)
 results = model.track(source="C:\\Users\\liamb\\Source\\Project Tony Stark\\YolfCV\\videoplayback (1).mp4", save=True)  # track an object in a video file
 
+Golfer_Track_ID = 1.0
+
+KeyPointNames = [
+    "nose", "left_eye", "right_eye", "left_ear", "right_ear",
+    "left_shoulder", "right_shoulder", "left_elbow", "right_elbow",
+    "left_wrist", "right_wrist", "left_hip", "right_hip",
+    "left_knee", "right_knee", "left_ankle", "right_ankle"
+]  
+
 for frame_result in results:
     boxes = frame_result.boxes
-
-    if boxes is None or len(boxes) == 0:
+    if boxes is None or boxes.id is None:
         continue
-    areas = (boxes.xywh[:, 2] * boxes.xywh[:, 3])
-    largest_idx = areas.argmax()
 
-    golfer_keypoints = frame_result.keypoints[largest_idx]
-    print(f"Detected {len(boxes)} people, using index {largest_idx}")
-    print(golfer_keypoints.shape)
+    ids = boxes.id.tolist()
+    if Golfer_Track_ID not in ids:
+        continue
+
+    golfer_idx = ids.index(Golfer_Track_ID)
+    golfer_keypoints = frame_result.keypoints.xy[golfer_idx]
+
+    keypoints_dict = {name: [round(x,2), round(y,2)] for name, (x,y) in zip(KeyPointNames, golfer_keypoints.tolist())}
+
+    print(keypoints_dict)
