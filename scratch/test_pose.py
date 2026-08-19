ff20ae7517c69f3477faa850d9038e748d8ba9b7
Liam <liamboyle637@outlook.com>
2026-08-19 12:56:20 -0400

    p6c3 pose testing 01

--- scratch/test_pose.py
+++ scratch/test_pose.py
@@ -2,8 +2,10 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n-pose.pt")  # load a pretrained model (recommended for training)
 results = model.track(source="C:\\Users\\liamb\\Source\\Project Tony Stark\\YolfCV\\videoplayback (1).mp4", save=True)  # track an object in a video file
+
 for frame_result in results:
     boxes = frame_result.boxes
+
     if boxes is None or len(boxes) == 0:
         continue
     areas = (boxes.xywh[:, 2] * boxes.xywh[:, 3])
